40d987b6d7b1f26e6e2512be66c7a0365f4dff10
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-10-27 22:09:32 -0500

    Works with a test file

--- src/main.py
+++ src/main.py
@@ -19,13 +19,13 @@ class Regex:
    
 class Col:
    ALL = [
-      "MovieID",
-      "UserID" ,
-      "Name"   ,
-      "Helpful",
-      "Score"  ,
-      "Time"   ,
-      "Summary",
+      "MovieID" ,
+      "UserID"  ,
+      "UserName",
+      "Helpful" ,
+      "Score"   ,
+      "Time"    ,
+      "Summary" ,
       "Text"
    ]
    
@@ -48,23 +48,24 @@ def Main(input_dir, output_dir):
    with open(output_path, 'wb') as f:
       csv_writer = csv.writer(f)
       csv_writer.writerow(Col.ALL)
-      Parse(input_path, 1000, _AppendReview, csv_writer)
+      Parse(input_path, 1000, _AppendReviews, csv_writer)
       
       
-def _AppendReview(review, csv_writer):
+def _AppendReviews(reviews, csv_writer):
    """
-      BRIEF  Add a single review to the file
+      BRIEF  Add the reviews to the file
    """
-   csv_writer.write([
-      review[Review.MOVIE_ID ],
-      review[Review.USER_ID  ],
-      review[Review.USER_NAME],
-      review[Review.HELPFUL  ],
-      review[Review.SCORE    ],
-      review[Review.TIME     ],
-      review[Review.SUMMARY  ],
-      review[Review.TEXT     ]
-   ])
+   for review in reviews:
+      csv_writer.writerow([
+         review[Review.MOVIE_ID ],
+         review[Review.USER_ID  ],
+         review[Review.USER_NAME],
+         review[Review.HELPFUL  ],
+         review[Review.SCORE    ],
+         review[Review.TIME     ],
+         review[Review.SUMMARY  ],
+         review[Review.TEXT     ]
+      ])
    
    
 if __name__ == '__main__':
